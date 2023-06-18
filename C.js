e3baeee2235fdc66bccae2dc900038e003c9f164
Alon Sobel <sobelalon@gmail.com>
2023-06-18 15:08:04 +0300

    connected checkbox and task description and connected edit and delete buttons

--- C.js
+++ C.js
@@ -37,24 +37,48 @@ const createDeleteTaskButtonElement = () => {
     return deleteTaskButton
 }
 
+const createEditTaskIconElement = () => {
+    const editTaskIcon = document.createElement("img");
+    editTaskIcon.src = "./assets/editIcon.png";
+    editTaskIcon.classList.add("editIcon");
+    editTaskIcon.addEventListener("click", editDescription());
+
+    return editTaskIcon;
+}
+
+const connectCheckboxAndText = (event) => {
+    const connector = document.createElement("div");
+    connector.appendChild(createNewCheckboxElement());
+    connector.appendChild(createTaskDescription(event));
+
+    return connector;
+}
+
+const connectEditAndDeleteButtons = () => {
+    const connector = document.createElement("div");
+    connector.appendChild(createEditTaskIconElement());
+    connector.appendChild(createDeleteTaskButtonElement());
+
+    return connector;
+}
+
 const clearTaskInput = (event) => event.target.value = "";
 
 const newTask = (event) => {
     const task = createNewTaskElement();
-    const checkbox = createNewCheckboxElement();
-    const taskDescription = createTaskDescription(event);
-    const deleteTaskButton = createDeleteTaskButtonElement();
+
+    const checkboxAndText = connectCheckboxAndText(event);
+    const editAndDeleteButtons = connectEditAndDeleteButtons();
     
-    task.appendChild(checkbox);
-    task.appendChild(taskDescription);
-    task.appendChild(deleteTaskButton);
+    task.appendChild(checkboxAndText);
+    task.appendChild(editAndDeleteButtons);
     
     todoList.appendChild(task);
     clearTaskInput(event);
 };
 
 const switchTaskList = (event) => {
-    const task = event.target.parentElement;
+    const task = event.target.parentElement.parentElement;
     event.target.checked ? moveToDone(task) : moveToTodo(task);
 };
 
@@ -72,6 +96,10 @@ const moveToTodo = (task) => {
     task.classList.add("toDoTask");
 };
 
-const deleteTask = (event) => event.target.parentElement.remove();
+const deleteTask = (event) => event.target.parentElement.parentElement.remove();
+
+const editDescription = () => {
+    
+}
 
 document.getElementById("taskDescriptionInput").addEventListener("change", newTask);
