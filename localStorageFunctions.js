bd93f1941826bcb92c9770bd251328868478f784
Alon Sobel <sobelalon@gmail.com>
2023-06-25 14:00:42 +0300

    seperated local storage functions to another file

--- localStorageFunctions.js
+++ localStorageFunctions.js
@@ -1,3 +1,70 @@
 const getTaskNumberFromLocalStorage = () => JSON.parse(localStorage.getItem("taskNumber")) || 1;
 
-export default { getTaskNumberFromLocalStorage };
+const getTasksFromLocalStorage = () => JSON.parse(localStorage.getItem("tasks")) || [];
+
+const updateLocalStorageAfterPositionSwap = (dragged, afterTask) => {
+    tasks = tasks.filter(task => task.id !== dragged.id);
+
+    if (afterTask === undefined) {
+        tasks.push(dragged);
+    } else {
+        tasks.splice(tasks.findIndex(task => task.id === afterTask.id), 0, dragged);
+    }
+
+    setTasksInLocalStorage();
+};
+
+const createTaskElementFromLocalStorage = (task) => {
+    const taskElement = createNewTaskElement(task.id, task.description);
+    specifyTaskList(task, taskElement);    
+};
+
+const initializeTasksFromLocalStorage = () => {
+
+    tasks.forEach(task => {
+        createTaskElementFromLocalStorage(task);
+    });
+};
+
+const addTaskToLocalStorage = (task) => {
+    const savedTask = createTaskObjectFromElement(task);
+
+    tasks.push(savedTask);
+
+    setTasksInLocalStorage();
+};
+
+const removeTaskFromLocalStorage = (event) => {
+    const taskId = getTaskElementFromHisChildren(event.target).id;
+
+    tasks = tasks.filter(task => task.id !== taskId);
+
+    setTasksInLocalStorage();
+};
+
+const updateTaskDescriptionInLocalStorage = (event) => {
+    const taskId = getTaskElementFromHisChildren(event.target).id;
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+    
+    tasks[taskIndex].description = event.target.value;
+    event.target.dataset.recentTaskDescription = event.target.value;
+
+    setTasksInLocalStorage();
+};
+
+const updateTaskStatusInLocalStorage = (event) => {
+    const taskId = getTaskElementFromHisChildren(event.target).id;
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+    tasks[taskIndex].list = event.target.checked ? "done" : "todo";
+
+    placeMovedTaskLastInArray(taskIndex);
+
+    setTasksInLocalStorage();
+};
+
+const setTasksInLocalStorage = () => localStorage.setItem("tasks", JSON.stringify(tasks));
+
+const RemoveAllListTasksFromLocalStorage = (listName) => {
+    tasks = tasks.filter(task => task.list !== listName);
+    setTasksInLocalStorage();
+};
--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-// import { getTaskNumberFromLocalStorage } from "./localStorageFunctions";
-
 const todoList = document.getElementById("todo");
 const doneList = document.getElementById("done");
 const lists = [todoList, doneList];
 
-const getTaskNumberFromLocalStorage = () => JSON.parse(localStorage.getItem("taskNumber")) || 1;
-
-let taskNumber = getTaskNumberFromLocalStorage();
-
-const getTasksFromLocalStorage = () => JSON.parse(localStorage.getItem("tasks")) || [];
+let taskNumber = getTaskNumberFromLocalStorage()
 
 let tasks = getTasksFromLocalStorage();
 
@@ -40,18 +34,6 @@ const createTaskObjectFromElement = (task) => {
     return task === undefined ? task : {id: task.id, 
         description: task.children[0].children[1].value,
         list: task.children[0].children[0].checked ? "done" : "todo"};
-}
-
-const updateLocalStorageAfterPositionSwap = (dragged, afterTask) => {
-    tasks = tasks.filter(task => task.id !== dragged.id);
-
-    if (afterTask === undefined) {
-        tasks.push(dragged);
-    } else {
-        tasks.splice(tasks.findIndex(task => task.id === afterTask.id), 0, dragged);
-    }
-
-    setTasksInLocalStorage();
 };
     
 setListsEventListeners();
@@ -215,18 +197,6 @@ const specifyTaskList = (task, taskElement) => {
     }
 };
 
-const createTaskElementFromLocalStorage = (task) => {
-    const taskElement = createNewTaskElement(task.id, task.description);
-    specifyTaskList(task, taskElement);    
-};
-
-const initializeTasksFromLocalStorage = () => {
-
-    tasks.forEach(task => {
-        createTaskElementFromLocalStorage(task);
-    });
-};
-
 initializeTasksFromLocalStorage();
 
 const clearTaskInput = (event) => event.target.value = "";
@@ -260,54 +230,11 @@ const getTaskElementFromHisChildren = (element) => {
     }
 
     return fatherElement;
-}
-
-const addTaskToLocalStorage = (task) => {
-    const savedTask = createTaskObjectFromElement(task);
-
-    tasks.push(savedTask);
-
-    setTasksInLocalStorage();
-};
-
-const removeTaskFromLocalStorage = (event) => {
-    const taskId = getTaskElementFromHisChildren(event.target).id;
-
-    tasks = tasks.filter(task => task.id !== taskId);
-
-    setTasksInLocalStorage();
-};
-
-const updateTaskDescriptionInLocalStorage = (event) => {
-    const taskId = getTaskElementFromHisChildren(event.target).id;
-    const taskIndex = tasks.findIndex(task => task.id === taskId);
-    
-    tasks[taskIndex].description = event.target.value;
-    event.target.dataset.recentTaskDescription = event.target.value;
-
-    setTasksInLocalStorage();
 };
 
 const placeMovedTaskLastInArray = (taskIndex) => {
     const movedTask = tasks.splice(taskIndex, 1);
     tasks.push(movedTask[0]);
-};
-
-const updateTaskStatusInLocalStorage = (event) => {
-    const taskId = getTaskElementFromHisChildren(event.target).id;
-    const taskIndex = tasks.findIndex(task => task.id === taskId);
-    tasks[taskIndex].list = event.target.checked ? "done" : "todo";
-
-    placeMovedTaskLastInArray(taskIndex);
-
-    setTasksInLocalStorage();
-};
-
-const setTasksInLocalStorage = () => localStorage.setItem("tasks", JSON.stringify(tasks));
-
-const RemoveAllListTasksFromLocalStorage = (listName) => {
-    tasks = tasks.filter(task => task.list !== listName);
-    setTasksInLocalStorage();
 };
 
 const clearListTasksDisplay  = (list) => {
